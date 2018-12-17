9417ea5cb80afb742d4587422910a02a40183ba0
Andrew <a.kr.andreev@gmail.com>
2018-12-17 23:46:01 +0200

    Update with second dice and input box for selecting end score

--- app.js
+++ app.js
@@ -12,25 +12,51 @@ var playersScore;
 var roundScore;
 var currentPlayer;
 var isGameFinished;
+var prevNum;
+var prevNum2;
+
+var finalScore;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click',function () {
 
   if (!isGameFinished){
-      //1. Get random number
-      var dice = Math.floor(Math.random()*6)+1;
-      //2. Display result
-      var diceDOM = document.querySelector('.dice');
-      diceDOM.style.display='block';
-      diceDOM.src = 'dice-' + dice + '.png';
-      //3. update score if not 0
-      if(dice !== 1){
-          roundScore+=dice;
-          document.querySelector('#current-' + currentPlayer).textContent=roundScore;
+
+      if (finalScore >0){
+          //1. Get random number
+          var dice =  Math.floor(Math.random()*6)+1;
+          var dice1 =  Math.floor(Math.random()*6)+1;
+          //2. Display result
+          var diceDOM = document.querySelector('.dice');
+          var diceDOM2 = document.querySelector('.dice1');
+          diceDOM.style.display='block';
+          diceDOM.src = 'dice-' + dice + '.png';
+          diceDOM2.style.display='block';
+          diceDOM2.src = 'dice-' + dice1 + '.png';
+          //3. update score if not 0
+          if(dice !== 1 && dice1 !== 1){
+
+              if (prevNum === 6 && dice === 6 || prevNum2 === 6 && dice1 === 6){
+                  playersScore[currentPlayer] = 0;
+                  document.querySelector('#score-' + currentPlayer).textContent = playersScore[currentPlayer];
+                  prevNum = -1;
+                  prevNum2 = -1;
+                  nextPlayer();
+              }
+
+              prevNum = dice;
+              prevNum2 = dice1;
+              roundScore+=(dice+dice1);
+              document.querySelector('#current-' + currentPlayer).textContent=roundScore;
+
+          }else {
+              nextPlayer();
+          }
       }else {
-          nextPlayer();
+          alert("Please, enter final score!")
       }
+
   }
 });
 
@@ -41,9 +67,10 @@ document.querySelector('.btn-hold').addEventListener('click',function () {
         //2. update UI
         document.querySelector('#score-' + currentPlayer).textContent = playersScore[currentPlayer];
         //3. check if player win
-        if (playersScore[currentPlayer]>=100){
+        if (playersScore[currentPlayer]>=finalScore){
             document.querySelector('#name-' + currentPlayer).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice1').style.display = 'none';
             document.querySelector('.player-'+currentPlayer + '-panel').classList.add('winner');
             document.querySelector('.player-'+currentPlayer + '-panel').classList.remove('active');
             isGameFinished=true;
@@ -57,7 +84,19 @@ document.querySelector('.btn-hold').addEventListener('click',function () {
 document.querySelector('.btn-new').addEventListener('click',init);
 
 
+document.getElementById("score-button").addEventListener('click',function () {
+    if (!isGameFinished){
+        finalScore =document.getElementById("new-final-value").value;
+        document.getElementById("new-final-value").value="";
+        init();
+    }
+
+});
+
 function init() {
+    //finalScore=100;
+     prevNum = -1;
+    prevNum2=-1;
      playersScore =[0,0];
      roundScore = 0;
      currentPlayer = 0;
@@ -67,6 +106,7 @@ function init() {
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.player-0-panel').classList.remove('winner');
@@ -86,5 +126,6 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
 
 }
